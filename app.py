1a3c312daccc60b98ee731f70865f2b6c619f0d7
KREITER <david.kreiter@hpe.com>
2026-01-20 16:46:21 +0200

    Change greet in main

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def greet(name):
     current_time = datetime.now().strftime("%H:%M")
-    return f"Hello, {name}! Current time is {current_time}"
+    return f"HHHHHello, {name}! Current time is {current_time}"
 
 if __name__ == "__main__":
     user = "World"
